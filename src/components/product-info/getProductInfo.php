85ddac633fdb550de53dc5d5933fdbc8e9e5d992
Ding <anluoridge@gmail.com>
2018-05-06 20:03:06 +1000

    [php] retrieve the data of selected product and show in a table

--- src/components/product-info/getProductInfo.php
+++ src/components/product-info/getProductInfo.php
@@ -1,34 +1,52 @@
 <?php
 
-//Procedural style
 $connection = mysqli_connect('rerun', 'potiro', 'pcXZb(kL', 'poti');
 
-// php can get the input directly from the HTML
-$made_after = $_REQUEST['earliest_made'];
-$made_before = $_REQUEST['latest_made'];
+$product_id = $_GET['product_id'];
+$query_string = "select * from products where product_id = ".$product_id;
 
-//$query_string = "select * from films where (year>=$made_after and
-// year <= $made_before)";
-$query_string = "select * from films";
+$result = mysqli_query($connection, $query_string);
+$num_rows = mysqli_num_rows($result);
+$items = mysqli_fetch_assoc($result);
 
-$result=mysqli_query($connection,$query_string);
-
-$num_rows=mysqli_num_rows($result);
-echo "Displaying the results using associative array";
-
-//using associative array
-// mysqli_fetch_assoc: This function will return a row as an associative array where the column names will be the keys storing corresponding value.
-
+print "<H1>Product Detail</H1>";
 if ($num_rows > 0 ) {
     print "<table border='0'>";
 
-	//write the code here to get values using associative array
-//    print "<td>$num_rows['title']</td>"
-//    print_r(array_keys($num_rows))
+    print "
+<tr>
+<td>Product: </td>
+<td>".$items["product_name"]."</td>
+</tr>
+
+<tr>
+<td>Price: </td>
+<td>"."$".$items["unit_price"]."</td>
+</tr>
+
+<tr>
+<td>Unit Quantity: </td>
+<td>".$items["unit_quantity"]."</td>
+</tr>
+
+";
+
     print "</table>";
+
+    print "<br>
+<table border='0'>
+<tr>
+<td><input type='text' size='3'></td>
+<td>"."/ ".$items["in_stock"]." available"."</td>
+</tr>
+
+<tr><td>　</td></tr>
+<tr>
+<td>
+<input type='submit' value='Add to cart'>
+</td>
+</tr>
+</table>";
 }
 
 mysqli_close($connection);
-
-
-?>
