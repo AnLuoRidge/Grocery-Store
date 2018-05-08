7d221f2097e59921d06dbfcec17c8063d88f0bbb
catherine <catherinechen427@gmail.com>
2018-05-08 16:31:17 +1000

    Homepage with three parts frames.

---
+++ src/components/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+        "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+
+<frameset cols="50%,*"  style="border-bottom-width: medium;border: #264BBC">
+<!-- Add navigation page path to the source here-->
+    <frame src="" name="leftFrame">
+    <frameset rows="50%,*" >
+<!--Add product details page to the source here-->
+        <frame src="form/Form.php" name="topRightFrame">
+<!--Add shopping cart page to the source here-->
+        <frame src="" name="bottomRightFrame">
+
+    </frameset>
+    <noframes></noframes>
+</frameset>
+
+</html>
